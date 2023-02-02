dd357497ec72a4ec6646ba2da8084d05d89aded4
mustafa854 <84233282+mustafa854@users.noreply.github.com>
2023-02-02 15:26:56 +0530

    Update index.tsx

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -4,16 +4,9 @@ import * as sc from './styled'
 
 export default function Skills() {
 
-  const skills = [
-    "React", "jQuery", "Vue", "Node.js", "Express", "Next.js", "Three.js"
-  ]
-
   return (
     <sc.MainWrapper>
-      
-      Feel free to take a look at my portfolio and get in touch if you'd like to discuss a project. 
-      
-     
+      Feel free to take a look at my portfolio and get in touch if you'd like to discuss a project.
       <br/>
       <br/>
       I&apos;d be more than happy to help bring your website dreams to life.
