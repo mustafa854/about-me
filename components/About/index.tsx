a983546cc4f40ffdb9d350b43eb9ea50fd2c26d5
mustafa854 <84233282+mustafa854@users.noreply.github.com>
2023-02-02 15:22:38 +0530

    Update index.tsx

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -7,7 +7,7 @@ export default function About() {
   return (
     <sc.MainWrapper>
       <p>
-        Hi, I&acute;m Mustafa Ashfaq Bidiwala, a talented website developer based in Surat, India. With a passion for technology and a keen eye for design, I bring a unique blend of skills to the table. I have a proven track record of delivering high-quality, user-friendly websites for a variety of clients.
+        Hi, I&acute;m Mustafa Bidiwala, a talented website developer based in Surat, India. With a passion for technology and a keen eye for design, I bring a unique blend of skills to the table. I have a proven track record of delivering high-quality, user-friendly websites for a variety of clients.
       </p>
       <p>
         Whether you&acute;re looking for a website for your business or a personal portfolio, I can help you bring your vision to life.
