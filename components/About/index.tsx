f7b3d632150748e3d95dc1bc4b82395dd285bfda
mustafa854 <84233282+mustafa854@users.noreply.github.com>
2023-02-02 13:58:25 +0530

    Update index.tsx

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -7,16 +7,15 @@ export default function About() {
   return (
     <sc.MainWrapper>
       <p>
-        My goal is to create pretty and reliable websites, where design and efficiency can be combined to provide the best user experience.
+        Hi, I'm Mustafa Ashfaq Bidiwala, a talented website developer based in India. With a passion for technology and a keen eye for design, I bring a unique blend of skills to the table. I have a proven track record of delivering high-quality, user-friendly websites for a variety of clients, and I am committed to staying up-to-date with the latest industry trends and technologies.
       </p>
       <p>
-        I&apos;m passionate about visual effects and anything motion related.
+        Whether you're looking for a website for your business or a personal portfolio, I can help you bring your vision to life. With a focus on clear communication, collaboration, and attention to detail, I work closely with each client to ensure their complete satisfaction.
       </p>
       <p>
-      My favorite frameworks are:
-      React, jQuery, Vue, Node.js, Express, Next.js and Three.js;
-
-      But I&apos;m open for new things.
+      Feel free to take a look at my portfolio and get in touch if you'd like to discuss a project. 
+      
+        I'd be more than happy to help bring your website dreams to life.
       </p>
     </sc.MainWrapper>
   )
